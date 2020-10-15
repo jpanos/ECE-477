589bfaf7ae58d30c0a734a3b71d56bbcfe13c488
iampizzaprincess <jacmalayter@gmail.com>
2020-10-15 19:12:40 -0400

    i2c with fixd header

--- stm/persephone/Common/Inc/i2c_battery.h
+++ stm/persephone/Common/Inc/i2c_battery.h
@@ -1,5 +1,16 @@
 #include "main.h"
 
+#ifndef __i2c_H
+#define __i2c_H
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
 void initI2C(void);
 void I2C1GPIOINIT(void);
 void I2C_StartTX(I2C_TypeDef* I2C, uint32_t DevAddress, uint8_t Size, uint8_t Direction);
+
+#ifdef __cplusplus
+}
+#endif
+#endif 
