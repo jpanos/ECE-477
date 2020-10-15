db38e780c91b27f36676e20c33f238152743da14
iampizzaprincess <jacmalayter@gmail.com>
2020-10-15 19:34:10 -0400

    i2c with fixed header againr

--- stm/persephone/Common/Inc/i2c_battery.h
+++ stm/persephone/Common/Inc/i2c_battery.h
@@ -1,16 +1,13 @@
+/* I2C initialization header for the battery monitor
+By Jackie
+*/
 #include "main.h"
 
-#ifndef __i2c_H
-#define __i2c_H
-#ifdef __cplusplus
- extern "C" {
-#endif
+#ifndef INC_I2C_BATTERY_H
+#define INC_I2C_BATTERY_H
 
 void initI2C(void);
 void I2C1GPIOINIT(void);
 void I2C_StartTX(I2C_TypeDef* I2C, uint32_t DevAddress, uint8_t Size, uint8_t Direction);
 
-#ifdef __cplusplus
-}
-#endif
-#endif 
+#endif /* INC_I2C_BATTERY_H */
