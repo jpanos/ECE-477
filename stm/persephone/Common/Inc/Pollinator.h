e3d8b184257c7a53ba06fcbb3508009b5bd594ee
msrasheed <moizr95@gmail.com>
2020-12-01 15:40:07 -0500

    jiggles up and down

--- stm/persephone/Common/Inc/Pollinator.h
+++ stm/persephone/Common/Inc/Pollinator.h
@@ -8,6 +8,11 @@
 #ifndef INC_POLLINATOR_H_
 #define INC_POLLINATOR_H_
 
+#define POLLINATOR_STRAIGHT_ANGLE   150
+#define POLLINATOR_UP_ANGLE         140
+#define POLLINATOR_DOWN_ANGLE       160
+#define POLLINATOR_DOWN_RANGE_ANGLE 180
+
 int check_sense(void);
 void set_angle(int);
 int get_angle(void);
